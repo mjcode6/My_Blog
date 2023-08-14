fa7e38ca803aa25bec6d1e90369533a6dd1518cb
sivasuthan manojan <mjcode6@gmail.com>
2023-08-14 20:33:17 +0200

    I worked on Adding fonction for admin

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,28 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+
+            return View("Add");
+        }
+    }
+}
