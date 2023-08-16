c5bef55ec84a0dc8c3dfe83f8f74404928b201fe
sivasuthan manojan <mjcode6@gmail.com>
2023-08-16 20:19:48 +0200

    I start to work ok admin add blog posts fonction

---
+++ Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,43 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            // Get tags from Repositories
+
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+
+            };
+
+
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
