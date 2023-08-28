49dee72bb1da69d639cf55314332387be99dcbb1
sivasuthan manojan <mjcode6@gmail.com>
2023-08-28 17:41:19 +0200

    i work on adding image for blog pôsts

---
+++ Repo/CloudinaryImageRepository.cs
@@ -0,0 +1,43 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Bloggie.Web.Repo
+{
+    public class CloudinaryImageRepository : IImageRepository
+    {
+        private readonly IConfiguration configuration;
+        private readonly Account account;
+
+        public CloudinaryImageRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            account = new Account(
+                configuration.GetSection("Cloudinary")["cloudName"],
+                configuration.GetSection("Cloudinary")["ApiKey"],
+                configuration.GetSection("Cloudinary")["ApiSecret"]
+
+                );
+        }
+
+      
+         public async Task<string> UploadAsync(IFormFile file)
+        {
+            var client = new Cloudinary(account);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                PublicId = "olympic_flag"
+            };
+            var uploadResult = await client.UploadAsync(uploadParams);
+
+            if(uploadResult != null && uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return uploadResult.SecureUri.ToString();
+            }
+#pragma warning disable CS8603 // Existence possible d'un retour de référence null.
+            return null;
+#pragma warning restore CS8603 // Existence possible d'un retour de référence null.
+        }
+    }
+}
